75c2d363368b3687786d297cae30c640300aa61d
Amadeus <38767929+Mozart409@users.noreply.github.com>
2022-05-17 11:59:38 +0200

    Update setup-new-machine.sh

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -18,7 +18,7 @@ do_space
 echo "Installing new packages"
 apt-get -q -y install fonts-firacode git zsh curl wget zip
 
-curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/master/zsh/.zshrc.sample > ~/.zshrc
+curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/zsh/.zshrc.sample > ~/.zshrc
 curl -L git.io/antigen > ~/antigen.zsh
 zsh
 chsh -s $(which zsh)
