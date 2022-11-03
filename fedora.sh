1d4bc8e9296415b690d78768ccada2e1dbe13c65
Amadeus <38767929+Mozart409@users.noreply.github.com>
2022-11-03 11:44:59 +0100

    feat: :hammer: updated scripts

--- fedora.sh
+++ fedora.sh
@@ -7,29 +7,20 @@ echo "Installing packages"
 # Install packages
 dnf install wget curl zip git fira-code-fonts util-linux-user util-linux zsh tmux -y
 dnf groupinstall "Development Tools" "Development Libraries" -y
+dnf install 'dnf-command(config-manager)' -y
 
 echo "zsh config"
 # ZSH
-curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/zsh/.zshrc.sample > ~/.zshrc
-curl -L git.io/antigen > ~/antigen.zsh
+curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/zsh/.zshrc.sample >~/.zshrc
+curl -L git.io/antigen >~/antigen.zsh
 
-echo "arkade"
-# Arkade
-curl -sLS https://get.arkade.dev | sudo sh
+echo "install aqua"
+curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.1.2/aqua-installer | bash
 
-echo "github cli"
-# Github Cli
-dnf install 'dnf-command(config-manager)' -y 
-sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo -y
-dnf install gh -y
+echo "Download aqua config"
+curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/aqua.yaml >~/.config/aqua/aqua.yaml
 
-echo "fnm"
-# fnm
-curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "./.fnm" --skip-shell
-
-
-echo "dprint"
-curl -fsSL https://dprint.dev/install.sh | sh
+aqua install --all
 
 echo "node"
 # node
--- setup-new-machine-debian.sh
+++ setup-new-machine-debian.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [ `whoami` != root ]; then
+if [ $(whoami) != root ]; then
     echo -e "Please run this script as root or using \e[31msudo"
     echo
     exit
 fi
 
-do_space(){
+do_space() {
     echo -e "\n \n"
 }
 
@@ -16,9 +16,18 @@ echo "Updating sources"
 apt update -y
 do_space
 echo "Installing new packages"
-apt-get -q -y install fonts-firacode git zsh curl wget zip build-essential 
+apt-get -q -y install fonts-firacode git zsh curl wget zip build-essential
 
-curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/zsh/.zshrc.sample > ~/.zshrc
-curl -L git.io/antigen > ~/antigen.zsh
+curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/zsh/.zshrc.sample >~/.zshrc
+curl -L git.io/antigen >~/antigen.zsh
 zsh
+
+echo "install aqua"
+curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.1.2/aqua-installer | bash
+
+echo "Download aqua config"
+curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/aqua.yaml >~/.config/aqua/aqua.yaml
+
+aqua install --all
+
 chsh -s $(which zsh)
