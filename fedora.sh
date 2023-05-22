d9a1a6a3630256ba40519a7a2519134f21d1afbe
Mozart409 <amadeus@mozart409.com>
2023-05-22 13:56:25 +0200

    fix: :hammer: changed fedora install script

--- fedora.sh
+++ fedora.sh
@@ -11,25 +11,24 @@ dnf update -y
 
 echo "Installing packages"
 # Install packages
-dnf install wget curl zip git fira-code-fonts util-linux-user util-linux zsh tmux -y
-dnf install google-cousine-fonts -y
+dnf install wget curl zip git netstat fira-code-fonts bat vim util-linux-user util-linux zsh tmux google-cousine-fonts -y
 dnf groupinstall "Development Tools" "Development Libraries" -y
 dnf install 'dnf-command(config-manager)' -y
 
 echo "zsh config"
 # ZSH
-curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/zsh/.zshrc.sample >~/.zshrc
-curl -L git.io/antigen >~/antigen.zsh
+curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/zsh/.zshrc.sample > ~/.zshrc
+curl -L git.io/antigen > ~/antigen.zsh
 
-echo "install aqua"
-curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.1.2/aqua-installer | bash
+# echo "install aqua"
+# curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.1.2/aqua-installer | bash
 
-echo "Download aqua config"
-curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/aqua.yaml >~/.config/aqua/aqua.yaml
+# echo "Download aqua config"
+# curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/aqua.yaml >~/.config/aqua/aqua.yaml
 
-aqua install --all
+# aqua install --all
 
-echo "node"
+# echo "node"
 # node
 # fnm install 18
 # fnm default 18
@@ -45,3 +44,9 @@ dnf install helix -y
 # install github cli
 dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo -y
 dnf install gh -y
+
+# install tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+
+# download docker script
+curl -fsSL https://get.docker.com -o ~/get-docker.sh
