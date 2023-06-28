dd20072504989a89056991f2d1f059e088367fc1
Amadeus <38767929+Mozart409@users.noreply.github.com>
2023-06-28 21:24:34 +0200

    Update setup-new-machine-debian.sh

--- setup-new-machine-debian.sh
+++ setup-new-machine-debian.sh
@@ -16,7 +16,7 @@ echo "Updating sources"
 apt update -y
 do_space
 echo "Installing new packages"
-apt-get -q -y install fonts-firacode git zsh curl wget zip build-essential
+apt-get -q -y install fonts-firacode git zsh curl wget zip build-essential vim neovim
 
 curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/zsh/.zshrc.sample >~/.zshrc
 curl -L git.io/antigen >~/antigen.zsh
