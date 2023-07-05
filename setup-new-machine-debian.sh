b532e5e6f271091b2d2ca388a4ade403d7f4232b
Amadeus <38767929+Mozart409@users.noreply.github.com>
2023-07-05 10:04:02 +0200

    Update setup-new-machine-debian.sh

--- setup-new-machine-debian.sh
+++ setup-new-machine-debian.sh
@@ -36,4 +36,10 @@ curl -fsSL https://tailscale.com/install.sh | sh
 # download docker script
 curl -fsSL https://get.docker.com -o ~/get-docker.sh
 
+apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+apt update
+apt install caddy
+
 chsh -s $(which zsh)
