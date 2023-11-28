dc42b87ef3798c81d29d4c462ff926e885c39509
Amadeus <maderamadeus@gmail.com>
2023-11-28 20:19:55 +0100

    fix: :wrench: Update setup-new-machine-debian.sh script

--- setup-new-machine-debian.sh
+++ setup-new-machine-debian.sh
@@ -19,8 +19,12 @@ echo "Installing new packages"
 apt-get -q -y install fonts-firacode git zsh curl wget zip build-essential net-tools vim neovim
 
 curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/zsh/.zshrc.sample >~/.zshrc
-curl -L git.io/antigen >~/antigen.zsh
+curl -L git.io/antigen > ~/antigen.zsh
+
 zsh
+
+chsh -s $(which zsh)
+
 
 # echo "install aqua"
 # curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.1.2/aqua-installer | bash
@@ -34,12 +38,12 @@ zsh
 curl -fsSL https://tailscale.com/install.sh | sh
 
 # download docker script
-curl -fsSL https://get.docker.com -o ~/get-docker.sh
+# curl -fsSL https://get.docker.com -o ~/get-docker.sh
 
-apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
-apt update
-apt install caddy
+# apt install -y debian-keyring debian-archive-keyring apt-transport-https
+# curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+# curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+# apt update
+# apt install caddy
 
-chsh -s $(which zsh)
+
