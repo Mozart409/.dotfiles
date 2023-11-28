952f8ecb1fc324bb512e3ca36a2c637eafe81832
Amadeus <maderamadeus@gmail.com>
2023-11-28 20:31:00 +0100

    feat: :technologist: Update package installations and add tmux configuration

--- setup-new-machine-debian.sh
+++ setup-new-machine-debian.sh
@@ -16,10 +16,14 @@ echo "Updating sources"
 apt update -y
 do_space
 echo "Installing new packages"
-apt-get -q -y install fonts-firacode git zsh curl wget zip build-essential net-tools vim neovim
+apt-get -q -y install fonts-firacode git zsh curl wget zip build-essential tmux net-tools vim neovim
 
 curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/zsh/.zshrc.sample >~/.zshrc
 curl -L git.io/antigen > ~/antigen.zsh
+
+# TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/main/.config/tmux/tmux.conf > ~/.config/tmux.conf
 
 zsh
 
