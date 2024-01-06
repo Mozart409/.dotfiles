d9714c0e4abe86754a76970d930b75ccc73dba48
Amadeus <38767929+Mozart409@users.noreply.github.com>
2024-01-06 15:02:21 +0100

    added MPR repo for just runner

--- setup-new-machine-debian.sh
+++ setup-new-machine-debian.sh
@@ -14,8 +14,16 @@ apt update -y
 
 apt install nala
 
+nala install -y --update wget
+
+# Add MPR Repo
+wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
+
+echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+
+
 echo "Installing new packages"
-nala install -y --update fonts-firacode git zsh curl wget zip build-essential tmux net-tools neovim ripgrep
+nala install -y --update fonts-firacode git zsh curl wget zip build-essential tmux net-tools neovim just ripgrep
 
 
 echo cloning .dotfiles in home / ~ dir
