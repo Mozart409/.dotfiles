3125f9020c717c854a529cf016b50cde9d1a98ca
Amadeus <38767929+Mozart409@users.noreply.github.com>
2024-03-03 12:59:21 +0100

    Update setup-new-machine-debian.sh

--- setup-new-machine-debian.sh
+++ setup-new-machine-debian.sh
@@ -17,9 +17,9 @@ apt install -y nala
 nala install -y --update wget gpg passwd
 
 # Add MPR Repo
-wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
+wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
 
-echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | tee /etc/apt/sources.list.d/prebuilt-mpr.list
 
 
 echo "Installing new packages"
