e0c3685a332c8e163df0b0460beba80fbef62d7d
Amadeus <38767929+Mozart409@users.noreply.github.com>
2024-03-03 23:17:33 +0100

    Update setup-new-machine-debian.sh

--- setup-new-machine-debian.sh
+++ setup-new-machine-debian.sh
@@ -14,11 +14,8 @@ apt update -y
 
 apt install -y nala
 
-nala install -y --update wget gpg passwd
-
 echo "Installing new packages"
-nala install -y --update fonts-firacode git zsh curl wget zip build-essential tmux net-tools neovim ripgrep jq sed unzip
-
+nala install -y --update wget gpg passwd fonts-firacode git zsh curl wget zip build-essential tmux net-tools neovim ripgrep jq sed unzip
 
 echo "cloning .dotfiles in home / ~ dir"
 git clone https://github.com/Mozart409/.dotfiles.git "$HOME"
