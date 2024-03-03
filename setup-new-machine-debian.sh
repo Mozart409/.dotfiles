2ba181a475a7588488341bd364e76dd6585bb119
Amadeus <38767929+Mozart409@users.noreply.github.com>
2024-03-03 13:12:57 +0100

    Update setup-new-machine-debian.sh

--- setup-new-machine-debian.sh
+++ setup-new-machine-debian.sh
@@ -16,14 +16,8 @@ apt install -y nala
 
 nala install -y --update wget gpg passwd
 
-# Add MPR Repo
-wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
-
-echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | tee /etc/apt/sources.list.d/prebuilt-mpr.list
-
-
 echo "Installing new packages"
-nala install -y --update fonts-firacode git zsh curl wget zip build-essential tmux net-tools neovim just ripgrep
+nala install -y --update fonts-firacode git zsh curl wget zip build-essential tmux net-tools neovim ripgrep jq sed
 
 
 echo "cloning .dotfiles in home / ~ dir"
