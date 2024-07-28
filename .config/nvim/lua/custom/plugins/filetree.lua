4a7bab16934b1571e47514afed1817194ef6d9ac
Attai <attai@amadeus.speedport.ip>
2024-07-28 10:24:40 +0200

    fix: :hammer: missing nvim-nio

--- .config/nvim/lua/custom/plugins/filetree.lua
+++ .config/nvim/lua/custom/plugins/filetree.lua
@@ -5,6 +5,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   version = "v3.x",
   dependencies = {
+    "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
